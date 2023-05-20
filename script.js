b96d1654b0dc2c42a9a7bd62d6c3c776e828dd03
aami_mm <mukarramos@outlook.com>
2023-05-20 19:36:02 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function delColor() {
+	var dropdown = document.getElementById("colorSelect");
+  var selectedIndex = dropdown.selectedIndex;
+	//alert("The selectedIndex is: " + selectedIndex)
+	dropdown.removeChild(dropdown.options[selectedIndex]);
+}
